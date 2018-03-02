60c5a25f45238ab8987bac5fb2f39e5965e127a4
EC2 Default User <ec2-user@ip-172-31-29-119.us-east-2.compute.internal>
2018-03-02 01:01:21 +0000

    Part 1 Finished

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,30 +11,71 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
-  end
+        @allRatings = Movie.all_ratings
 
+        if(params[:sort] != nil and params[:sort] != session[:sort])
+          session[:sort] = params[:sort]
+        end
+        
+        if(params[:ratings] != nil and params[:ratings] != session[:ratings])
+        session[:ratings] = params[:ratings]
+        end
+
+        session_params = { :sort => session[:sort], :ratings => session[:ratings] }
+        param_params = { :sort => params[:sort], :ratings => params[:ratings] }
+
+        if(session_params != param_params)
+          flash.keep
+          redirect_to(session_params)
+        end
+
+
+        if(session[:ratings] != nil)
+          @selected_ratings =session[:ratings].keys
+        else
+          @selected_ratings =Movie.all_ratings
+        end
+
+        if(session[:sort]=='title')
+          if (session[:ratings] )
+            @movies = Movie.where("rating in (?)", @selected_ratings).order('title')
+          else
+            @movies = Movie.all.order('title')
+          end
+        elsif(session[:sort]=='release_date')
+          if (session[:ratings])
+            @movies = Movie.where("rating in (?)", @selected_ratings).order('release_date')
+          else
+            @movies = Movie.all.order('release_date')
+          end
+        else
+          if (session[:ratings] )
+            @movies = Movie.where("rating in (?)", @selected_ratings)
+          else
+          @movies = Movie.all
+          end
+        end
+        @title_header = params[:sort]=='title' ?'high-light':nil
+        @release_date_header = params[:sort]=='release_date' ?'high-light':nil
+  end
+  
   def new
     # default: render 'new' template
   end
-
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
-
   def edit
     @movie = Movie.find params[:id]
   end
-
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
-
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+  def Movie.all_ratings
+    all_movie_ratings = []
+    Movie.all.each do |movie|
+      if (!all_movie_ratings.include?(movie.rating))
+        all_movie_ratings.push(movie.rating)
+      end
+    end
+    return all_movie_ratings
+  end
 end
