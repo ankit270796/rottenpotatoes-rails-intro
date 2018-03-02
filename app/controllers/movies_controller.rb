831257db238b8e25aee7bad8fd3af7c77391c4a1
EC2 Default User <ec2-user@ip-172-31-29-119.us-east-2.compute.internal>
2018-03-02 01:06:03 +0000

    Part 2 Finished

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -62,20 +62,24 @@ class MoviesController < ApplicationController
   def new
     # default: render 'new' template
   end
+  
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
+  
   def edit
     @movie = Movie.find params[:id]
   end
+  
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
+  
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
